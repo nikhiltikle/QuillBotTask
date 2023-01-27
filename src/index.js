41ae59fd7db03ce92af8acf588b863eaf5610dbc
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-01-27 18:40:26 +0530

    Add Mui theme and basic components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme/mui/theme';
+import Dashboard from './pages/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SideNavbar from '../../components/SideNavbar';
+
+export default function Dashboard() {
+  return (
+    <>
+      <SideNavbar />
+    </>
+  );
+}
