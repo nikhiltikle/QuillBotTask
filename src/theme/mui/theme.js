e94b3ab7dd5d5d2d06a70eb9322bfeca5faef84a
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-01-27 20:42:39 +0530

    Create theme for drawer and body

--- src/theme/mui/theme.js
+++ src/theme/mui/theme.js
@@ -1,3 +1,47 @@
 import { createTheme } from '@mui/material';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#273244',
+    },
+  },
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: '#1F2A3C',
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: '#D4D7DD',
+          fontFamily: 'Open sans, sans-serif',
+        },
+      },
+    },
+    MuiDivider: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#394B61',
+        },
+      },
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          color: '#D4D7DD',
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#1F2A3C',
+        },
+      },
+    },
+  },
+});
