5123d70431d27fc224e326d1801d2292efef57b4
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-01-30 16:42:11 +0530

    Show movie details above movie card

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
-import { CardMedia, CardContent, CardActions, Card, Box } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Zoom from '@mui/material/Zoom';
 import Typography from '@mui/material/Typography';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import { styles, SvgCardButton } from './styles';
 import MovieDetail from '../MovieDetail';
 
+const CARDS_GAP = 37;
+
 export default function MovieList({ movies }) {
   const [selectedMovie, setMovie] = useState({});
+  const [movieDetailOrder, setMovieDetailOrder] = useState(0);
 
   const classes = styles();
 
-  const onSelectMovie = (movie) => {
+  const calculateMovieDetailOrder = (index) => {
+    const container = document.getElementById('moviesList');
+    const movieCard = document.getElementById('movieCard');
+    const containerWidth = container.clientWidth;
+    const movieCardWidth = movieCard.offsetWidth + CARDS_GAP;
+
+    const cardsInRow = Math.round(containerWidth / movieCardWidth);
+
+    const order = Math.floor(index / cardsInRow) * cardsInRow;
+
+    setMovieDetailOrder(order);
+  };
+
+  const onSelectMovie = (movie, index) => {
+    calculateMovieDetailOrder(index);
     setMovie(movie);
   };
 
@@ -20,29 +42,33 @@ export default function MovieList({ movies }) {
       sx={{
         display: 'flex',
         flexWrap: 'wrap',
-        gap: '37px 26px',
+        gap: `${CARDS_GAP}px 26px`,
         width: '100%',
       }}
+      id="moviesList"
     >
+      {selectedMovie?.imdbID && (
+        <Zoom key={selectedMovie?.imdbID} in timeout={800}>
+          <Box sx={{ flex: '1 1 100%', order: movieDetailOrder }}>
+            <MovieDetail
+              title={selectedMovie.Title}
+              images={selectedMovie.Poster}
+              runTime={selectedMovie.Runtime}
+              year={selectedMovie.Year}
+              directedBy={selectedMovie.Director}
+              language={selectedMovie.Language}
+              plot={selectedMovie.Plot}
+              imdbRating={selectedMovie.imdbRating}
+            />
+          </Box>
+        </Zoom>
+      )}
       {movies.map((movie, index) => (
         <>
-          {selectedMovie?.imdbID === movie?.imdbID && (
-            <Box sx={{ flex: '1 1 100%' }}>
-              <MovieDetail
-                title={movie.Title}
-                images={movie.Poster}
-                runTime={movie.Runtime}
-                year={movie.Year}
-                directedBy={movie.Director}
-                language={movie.Language}
-                plot={movie.Plot}
-                imdbRating={movie.imdbRating}
-              />
-            </Box>
-          )}
           <Card
-            sx={{ ...classes.mainCard }}
-            onClick={() => onSelectMovie(movie)}
+            id="movieCard"
+            sx={{ ...classes.mainCard, order: index }}
+            onClick={() => onSelectMovie(movie, index)}
           >
             <CardMedia
               sx={classes.image}
